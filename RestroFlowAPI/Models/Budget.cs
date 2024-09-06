c2a79aad4721fa72368c3241d122db8f73c2a225
Brian <briannguyenwg@gmail.com>
2024-09-07 02:29:35 +1000

    modify budget table

--- RestroFlowAPI/Models/Budget.cs
+++ RestroFlowAPI/Models/Budget.cs
@@ -16,7 +16,9 @@ namespace RestroFlowAPI.Models
     [Column(TypeName = "decimal(18,2)")]
     public decimal BudgetAmount { get; set; }
     public string? Description { get; set; }
-    public required string BudgetTimePeriod { get; set; }
+    // Weekly period represented by start and end dates
+    public DateTime BudgetStartDate { get; set; }  // Start of the week
+    public DateTime BudgetEndDate { get; set; }    // End of the week
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
 
